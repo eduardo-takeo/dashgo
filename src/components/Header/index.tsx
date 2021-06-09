e74d4bcf4f7626ffb5902168df59e826cc75db9d
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-09 18:49:21 -0300

    Add responsive Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 import { SearchBox } from "./SearchBox";
 import { Logo } from "./Logo";
 import { Profile } from "./Profile";
 import { NotificationsNav } from "./NotificationsNav";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export default function Header() {
+  const { onOpen } = useSidebarDrawer();
   const isWidescreen = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +25,16 @@ export default function Header() {
       px="6"
       align="center"
     >
+      {!isWidescreen && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
       <Logo />
       {isWidescreen && <SearchBox />}
 
