d9d298e6edfb73d20cb46a7a85a99b3390e7170a
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-16 21:42:03 -0300

    Fix small typo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -66,7 +66,7 @@ export default function Dashboard() {
       <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid flex="1" gap="4" minChildWidth="320px" aliPgn="flex-start">
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
           <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="large" mb="4">
               Inscritos da semana
